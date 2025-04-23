0c5130afc9139d42ecef46b562cfc4433af5a69e
gamzekandara <gamzekandara@gmail.com>
2025-04-23 17:23:14 -0400

    Add recursivesearch

--- javascript/recursive_search.js
+++ javascript/recursive_search.js
@@ -1,6 +1,15 @@
 function recursiveSearch(arr, target) {
-  // type your code here
+  if (arr.length === 0) {
+    return false;
+  }
+
+  if (arr[0] === target) {
+    return true;
+  }
+
+  return recursiveSearch(arr.slice(1), target);
 }
+
 
 if (require.main === module) {
   // add your own tests in here
@@ -16,4 +25,11 @@ if (require.main === module) {
 module.exports = recursiveSearch;
 
 // Please add your pseudocode to this file
+// given an array of values, use recursion to find the target value, return true if found, else false.
+
+// Base case: if the array is empty => return false.
+// start with the first element
+// compare with the target value
+// call recursiveSearch function for the rest of the array arr[1:len -1]
+
 // And a written explanation of your solution
